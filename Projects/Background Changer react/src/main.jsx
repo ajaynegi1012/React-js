7ff4189a5cc998d0628eca32f41c43d09d7565fb
ajaynegi1012 <ajaysinghn164@gmail.com>
2026-05-03 01:31:02 +0530

    password generator structure started

--- Projects/Background Changer react/src/main.jsx
+++ Projects/Background Changer react/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ColorChanger from './SelfPractice/ColorChanger.jsx'
+import PasswordGenerator from './projects/PasswordGenerator/PasswordGenerator.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <ColorChanger />
+    {/* <ColorChanger /> */}
+    <PasswordGenerator />
   </StrictMode>,
 )
---
+++ Projects/Background Changer react/src/projects/PasswordGenerator/PasswordGenerator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PasswordGenerator() {
+    return (
+        <>
+        <h1 className='text-4xl text-green-500'>Password Ganerator</h1>
+        </>
+    )
+}
+
+export default PasswordGenerator
