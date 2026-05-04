14aca3b215d896e91ac60a7e2c3c25aaa2290589
ajaynegi1012 <ajaysinghn164@gmail.com>
2026-05-05 01:26:38 +0530

    music player initial

--- Projects/Background Changer react/src/main.jsx
+++ Projects/Background Changer react/src/main.jsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import ColorChanger from './SelfPractice/ColorChanger.jsx'
 import PasswordGenerator from './projects/PasswordGenerator/PasswordGenerator.jsx'
+import MusicPlayer from './projects/MusicPlayer/MusicPlayer.jsx'
+import './assets/style/MusicPlayer.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
     {/* <ColorChanger /> */}
-    <PasswordGenerator />
+    {/* <PasswordGenerator /> */}
+    <MusicPlayer />
   </StrictMode>,
 )
---
+++ Projects/Background Changer react/src/projects/MusicPlayer/MusicPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MusicPlayer() {
+    return (
+        <div>This is Music player</div>
+    )
+}
+
+export default MusicPlayer
