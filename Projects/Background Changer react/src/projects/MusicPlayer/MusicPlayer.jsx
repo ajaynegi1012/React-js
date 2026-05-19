c302af83a90e6a3af1fc459fddf3443c69050ceb
ajaynegi1012 <ajaysinghn164@gmail.com>
2026-05-20 00:39:36 +0530

    icons added

--- Projects/Background Changer react/src/projects/MusicPlayer/MusicPlayer.jsx
+++ Projects/Background Changer react/src/projects/MusicPlayer/MusicPlayer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../../assets/style/MusicPlayer.css';
 import headPhoneImg from '../../assets/images/headPhone.jpeg';
-import playpause from '../../assets/images/play-button.png';
+import previousIcon from '../../assets/images/previous.png';
+import playIcon from '../../assets/images/play-button.png';
+import nextIcon from '../../assets/images/next.png';
+import pauseIcon from '../../assets/images/pauseBtn.png';
+import loopIcon from '../../assets/images/loop.png';
+import clockIcon from '../../assets/images/clock.png';
 
 function MusicPlayer() {
     return (
@@ -10,23 +15,31 @@ function MusicPlayer() {
                 <div className="img-container">
                     <img className='img' src={headPhoneImg} alt="img" />
                 </div>
-                    {/* <span className='song-track'>Song track</span> */}
-                    <hr className='song-track' />
+                {/* <span className='song-track'>Song track</span> */}
+                <hr className='song-track' />
                 <div className="bottom-container">
                     <div className="controller-container">
-                        <span>loop</span>
+                        <span>
+                            <img src={loopIcon} className='media-controls' />
+                        </span>
                         <div className="play-pause-container">
                             <button>
-                                <img src={playpause} className='play-pause' />
+                                <img src={previousIcon} id='previous-btn' className='media-controls' />
                             </button>
                             <button>
-                                <img src={playpause} className='play-pause' />
+                                {
+                                    ""
+                                        ?
+                                        <img src={playIcon} id='playIcon' className='media-controls' />
+                                        :
+                                        <img src={pauseIcon} id='pauseIcon' className='media-controls' />
+                                }
                             </button>
                             <button>
-                                <img src={playpause} className='play-pause' />
+                                <img src={nextIcon} className='media-controls' />
                             </button>
                         </div>
-                        <span>timer</span>
+                        <span><img src={clockIcon} className='media-controls' /></span>
                     </div>
                 </div>
             </div>
