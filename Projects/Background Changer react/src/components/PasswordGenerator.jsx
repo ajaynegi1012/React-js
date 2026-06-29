435b663b018956730c4514c01b3cdab85922bf0b
ajaynegi1012 <ajaysinghn164@gmail.com>
2026-06-29 22:27:53 +0530

    basic structure ready

--- Projects/Background Changer react/src/components/PasswordGenerator.jsx
+++ Projects/Background Changer react/src/components/PasswordGenerator.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 function PasswordGenerator() {
+
+  const [length, setLength] = useState(8);  //For length feild
+  const [numAllowed, setNumAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTWXZYabcdefgijklmnopqrstwxyz";
+    if(numAllowed){
+      str = str + "0123456789";
+    }
+    if(charAllowed){
+      str = str + "!@#$%^&*-_+=[]{}`~";
+    }
+    
+  }, [length, numAllowed, charAllowed, setPassword])
+
   return (
-    <div>PasswordGenerator</div>
+    <>
+      <h3 className="text-4xl text-center text-white ">PasswordGenerator</h3>
+    </>
   )
 }
 
