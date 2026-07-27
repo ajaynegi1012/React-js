b181a6fb7ebf547f51ba2f4ba46a372e77b0589e
ajaynegi1012 <ajaysinghn164@gmail.com>
2026-07-28 00:43:16 +0530

    starting it back

--- Projects/Background Changer react/src/components/PasswordGenerator.jsx
+++ Projects/Background Changer react/src/components/PasswordGenerator.jsx
@@ -5,30 +5,41 @@ function PasswordGenerator() {
   const [length, setLength] = useState(8);  //For length feild
   const [numAllowed, setNumAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("password");
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTWXZYabcdefgijklmnopqrstwxyz";
-    if(numAllowed){
+    if (numAllowed) {
       str = str + "0123456789";
     }
-    if(charAllowed){
+    if (charAllowed) {
       str = str + "!@#$%^&*-_+=[]{}`~";
     }
 
     for (let i = 1; i < array.length; i++) {
-      let char = Math.floor(Math.random() * str.length()+1);  //+1 is because as index start with 0.
-      pass = str.charAt(char);  
+      let char = Math.floor(Math.random() * str.length() + 1);  //+1 is because as index start with 0.
+      pass = str.charAt(char);
     }
 
     setPassword(pass);
-    
+
   }, [length, numAllowed, charAllowed, setPassword])
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto rounded-lg p-4 my-8 text-white bg-gray-400 text-center text-4xl'>test</div>
+      <div className='w-full max-w-md mx-auto rounded-lg px-4 my-8 text-black bg-gray-700 text-center text-4xl'>
+        <h1 className='text-white' >Password Generator</h1>
+        <div className='flex shadow overflow-hidden'>
+          <input
+            type="text"
+            value={password}
+            className='outline-none w-full p-3 rounded-lg my-4 bg-white' //Y : top and bottom // x : left and right
+            placeholder='password'
+            readOnly //No changes can be done.
+          />
+        </div>
+      </div>
     </>
   )
 }
